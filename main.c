5db3582cd13045bb80c56cfb8b67a516825c6b6c
joacoferna <jafernandez1@miuandes.cl>
2017-09-26 00:33:04 -0300

    Starting from the begining

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include "child.h"
 
 // Cuenten con este codigo monolitico en una funcion
 // main como punto de partida.
@@ -13,16 +15,20 @@
 // e incluso en archivos separados, con dependencias
 // distribuidas en headers. Pueden modificar el Makefile
 // libremente para lograr esto.
-
-
 int main(int argc, char** argv) {
 	size_t bufsize = 512;
 	char* commandBuf = malloc(sizeof(char)*bufsize);
+
+	// Para guardar descriptores de pipe
+	// el elemento 0 es para lectura
+	// y el elemento 1 es para escritura.
+	int bankPipe[2];
+	char readbuffer[80]; // buffer para lectura desde pipe
+
+	// Se crea un pipe...
+	pipe(bankPipe);
+
 	const int bankId = getpid() % 1000;
-	Suc* suc_array = calloc(50, sizeof(Suc));
-	int array_size = 0;
-	Params* par = calloc(1, sizeof(Params));
-	par->suc_array = suc_array;
 	printf("Bienvenido a Banco '%d'\n", bankId);
 
 	while (true) {
@@ -45,70 +51,40 @@ int main(int argc, char** argv) {
 			pid_t sucid = fork();
 
 			if (sucid > 0) {
-				printf("%d hiya! 1\n", sucid);
-				static int N;
-				char* n = &commandBuf[4];
-				N = atoi(n);
-				if(!N) N = 1000;
-				printf("Sucursal creada con ID '%d' y %d cuentas\n", sucid, N);
-				
-				array_size++;
-				par->sucid = sucid;
-				par->clients = N;
-				par->array_size = array_size;
-				Init_suc((void*)par);
+				printf("Sucursal creada con ID '%d'\n", sucid);
+
+				// Enviando saludo a la sucursal
+				char msg[] = "Hola sucursal, como estas?";
+				write(bankPipe[1], msg, (strlen(msg)+1));
+
 				continue;
 			}
 			// Proceso de sucursal
 			else if (!sucid) {
 				int sucId = getpid() % 1000;
 				printf("Hola, soy la sucursal '%d'\n", sucId);
-				par->array_size = array_size;
-				par->sucid = sucId;
-				par->sucursal = Find_suc((void*)par);
-				Exec_suc((void*)par);
-				//usleep(1000000);
-				// 100 milisegundos...
-				/*int bytes = read(bankPipe[0], readbuffer, sizeof(readbuffer));
-				printf("Soy la sucursal '%d' y me llego mensaje '%s' de '%d' bytes.\n",
-				sucId, readbuffer, bytes);*/
+				while (true) {
+					// 100 milisegundos...
+					int bytes = read(bankPipe[0], readbuffer, sizeof(readbuffer));
+					printf("Soy la sucursal '%d' y me llego mensaje '%s' de '%d' bytes.\n",
+							sucId, readbuffer, bytes);
 
-				// Usar usleep para dormir una cantidad de microsegundos
+					// Usar usleep para dormir una cantidad de microsegundos
+					// usleep(100000);
 
-				// Cerrar lado de lectura del pipe
+					// Cerrar lado de lectura del pipe
+					close(bankPipe[0]);
 
-				// Para terminar, el proceso hijo debe llamar a _exit,
-				// debido a razones documentadas aqui:
-				// https://goo.gl/Yxyuxb
-				//_exit(EXIT_SUCCESS);
+					// Para terminar, el proceso hijo debe llamar a _exit,
+					// debido a razones documentadas aqui:
+					// https://goo.gl/Yxyuxb
+					_exit(EXIT_SUCCESS);
+				}
 			}
 			// error
 			else {
 				fprintf(stderr, "Error al crear proceso de sucursal!\n");
 				return (EXIT_FAILURE);
-			}
-		}
-		else if(!strncmp("list", commandBuf, strlen("list"))){
-			for(int i = 0; i < array_size; i++){
-				printf("%d, clients amount: %d:", suc_array[i].ID, suc_array[i].clients_amount);
-				for(int j = 0; j < suc_array[i].clients_amount; j++){
-					printf("%6d ",suc_array[i].accountid[j]);
-				}
-			printf("\n");
-			}
-		}
-		else if(!strncmp("kill", commandBuf, strlen("kill"))){
-			int id = atoi(&commandBuf[4]);
-			if(!id){
-				printf("El comando ''kill'' debe ser ingresado junto con un id valido.");
-			}
-			else{
-				par->sucid = id;
-				Suc dead_sucursal = Find_suc((void*)par);
-				if (!dead_sucursal.ID) printf("El comando ''kill'' debe ser ingresado junto con un id valido: id no encontrado.");
-				else{
-					write(dead_sucursal.pipein[1], "die", 3);
-				}
 			}
 		}
 		else {
@@ -119,6 +95,7 @@ int main(int argc, char** argv) {
 
 	printf("Terminando ejecucion limpiamente...\n");
 	// Cerrar lado de escritura del pipe
+	close(bankPipe[1]);
 
 	return(EXIT_SUCCESS);
 }
